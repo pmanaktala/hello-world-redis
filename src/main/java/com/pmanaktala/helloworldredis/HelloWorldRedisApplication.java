2c292e0454173e7bb68d1817f097e3db918b52f4
Parth Manaktala <manaktala.parth@gmail.com>
2021-12-04 18:25:42 +0530

    Added project structure

---
+++ src/main/java/com/pmanaktala/helloworldredis/HelloWorldRedisApplication.java
@@ -0,0 +1,13 @@
+package com.pmanaktala.helloworldredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldRedisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pmanaktala/helloworldredis/HelloWorldRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pmanaktala.helloworldredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
